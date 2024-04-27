ec4a3a844dd74b70c390f1cab75c085be4c0ac84
1221jyp <98996860+1221jyp@users.noreply.github.com>
2024-04-28 01:51:57 +0900

    add mysql

--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+require("dotenv").config();
+console.log(process.env);
+const connection = mysql.createConnection({
+  host: "www.1uton.com",
+  user: process.env.MySQLId,
+  password: process.env.MySQLPassword,
+  database: process.env.MySQLDBname,
+});
+
+connection.connect();
+
+connection.query("SELECT * from topic", (error, rows, fields) => {
+  if (error) throw error;
+  console.log("User info is: ", rows);
+});
+
+connection.end();
 
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
