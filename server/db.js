9ae24632778817e075189f6c6ecc4ac2780c49c7
ihan <ihan43360@gamil.com>
2024-06-12 15:15:49 +0900

    ihan

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "www.1uton.com",
+  host: "1uton.com",
   user: process.env.MySQLId,
   password: process.env.MySQLPassword,
   database: process.env.MySQLDBname,
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const connection = require("./db");
+const { request } = require("http");
 require("dotenv").config();
 console.log(process.env);
 
@@ -27,7 +28,29 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
+app.get("/api/ihan", (req, res) => {
+  res.sendFile(path.join(__string, "../client/build", "index.html"));
+});
 
+app.get( '/api/create', function(request, response){
+  { fs.readdir(' ./data', function(error, filelist){
+    var title = 'WEB - create';
+    var list = template. list(filelist); 
+    var html = template.HTML(title, list, 
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p> 
+        <p> 
+          <textarea name="description" placeholder="descrilption"></textarea> 
+        </p> 
+        <p> 
+          <input type="submit"> 
+        </p> 
+      </form>
+    ,'');
+   response.writeHead(200); 
+   response. end(html);
+   
+app.ger(funtion(res))
 app.listen(5500, () => {
   console.log("Server running in port 5500");
 });
